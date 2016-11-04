1fd3c96e24a6d5a5001eee86703ba6e91a6a39be
pf-siedler <pferd262@gmail.com>
2016-11-04 12:48:48 +0900

    Add emacs init.el

---
+++ init.el
@@ -0,0 +1,14 @@
+;;設定ファイルテスト用
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+;;El-Getインストール設定
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(el-get-bundle auto-complete)

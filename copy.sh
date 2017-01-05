5575976f4379b8c6f192f6518b0aff60251487f0
pf-siedler <pferd262@gmail.com>
2017-01-05 17:36:29 +0900

    Add nano config file and rename .hoge to hoge

--- copy.sh
+++ copy.sh
@@ -1,8 +1,8 @@
  #!/bin/bash
 
-DOT_FILES=(.zshrc .tmux.conf .emacs.d/init.el .emacs.d/conf/org.el .emacs.d/conf/interface.el .emacs.d/message.org)
+DOT_FILES=(zshrc tmux.conf emacs.d/init.el emacs.d/conf/org.el emacs.d/conf/interface.el nanorc)
 
 for file in ${DOT_FILES[@]}
 do
-     ln -sf $HOME/dotfiles/$file $HOME/$file
+     ln -sf $HOME/dotfiles/$file $HOME/.$file
 done

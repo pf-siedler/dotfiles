8718d18b7c70e821a5f665066486ebc107362937
Takuma Suzuki <pferd262@gmail.com>
2022-11-12 15:23:01 +0900

    Update

--- home.nix
+++ home.nix
@@ -16,8 +16,8 @@
   # changes in each release.
   home.stateVersion = "22.05";
 
-  #imports = [ (import <codex>).hmModule.${builtins.currentSystem} ];
-  #codex.enable = true;
+  imports = [ (import <codex>).hmModule.${builtins.currentSystem} ];
+  codex.enable = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;

d3f1f31a372552d04b1277b1f299860547588b6c
Takuma Suzuki <pferd262@gmail.com>
2022-12-01 17:00:02 +0900

    私用PCの設定を社用とは別に作る

--- flake.nix
+++ flake.nix
@@ -6,12 +6,13 @@
     codex.url = "path:/Users/pfsiedler/herp/codex";
   };
 
-  outputs = { self, nixpkgs, home-manager, codex, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, codex ? {}, ... }@inputs: {
     homeConfigurations.herp = let
       system = "x86_64-darwin";
       pkgs = nixpkgs.legacyPackages.x86_64-darwin;
     in home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
+      extraSpecialArgs = { inherit inputs; };
       modules = [
         ./home.nix
         codex.hmModule.x86_64-darwin
@@ -19,6 +20,20 @@
           home.username = "pfsiedler";
           home.homeDirectory = "/Users/pfsiedler";
           codex.enable = true;
+        })
+      ];
+    };
+    homeConfigurations.surface = let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      extraSpecialArgs = { inherit inputs; };
+      modules = [
+        ./home.nix
+        ({ ... }: {
+          home.username = "pfsiedler";
+          home.homeDirectory = "/home/pfsiedler";
         })
       ];
     };

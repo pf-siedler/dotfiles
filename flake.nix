f28a7209a075cc1abab83701327b33b77bb42db8
Takuma Suzuki <pferd262@gmail.com>
2022-12-09 17:35:35 +0900

    開発環境を flake で準備

--- flake.nix
+++ flake.nix
@@ -5,39 +5,45 @@
     home-manager.url = "github:nix-community/home-manager";
     codex.url =
       "github:herp-inc/codex/e92a3f38bd777a1e24b2d4308d302ebf5a7b0688";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, home-manager, codex ? { }, ... }@inputs: {
-    homeConfigurations.herp = let
-      system = "x86_64-darwin";
-      pkgs = nixpkgs.legacyPackages.x86_64-darwin;
-    in home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = { inherit inputs; };
-      modules = [
-        ./home.nix
-        codex.hmModule.x86_64-darwin
-        ({ ... }: {
-          home.username = "pfsiedler";
-          home.homeDirectory = "/Users/pfsiedler";
-          home.packages = [ pkgs.cachix ];
-          codex.enable = true;
-        })
-      ];
-    };
-    homeConfigurations.surface = let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = { inherit inputs; };
-      modules = [
-        ./home.nix
-        ({ ... }: {
-          home.username = "pfsiedler";
-          home.homeDirectory = "/home/pfsiedler";
-        })
-      ];
-    };
-  };
+  outputs = { self, nixpkgs, home-manager, flake-utils, codex, ... }@inputs:
+    {
+      homeConfigurations.herp = let
+        system = "x86_64-darwin";
+        pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+      in home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit inputs; };
+        modules = [
+          ./home.nix
+          codex.hmModule.x86_64-darwin
+          ({ ... }: {
+            home.username = "pfsiedler";
+            home.homeDirectory = "/Users/pfsiedler";
+            home.packages = [ pkgs.cachix ];
+            codex.enable = true;
+          })
+        ];
+      };
+      homeConfigurations.surface = let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit inputs; };
+        modules = [
+          ./home.nix
+          ({ ... }: {
+            home.username = "pfsiedler";
+            home.homeDirectory = "/home/pfsiedler";
+          })
+        ];
+      };
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell { buildInputs = [ pkgs.gnumake pkgs.nixfmt ]; };
+      });
 }
